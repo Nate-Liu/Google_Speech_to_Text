e0bae8d6fec3e7b02fde70ccd9642b1bde93b424
Nate Liu <32503070+Nate-Liu@users.noreply.github.com>
2019-08-28 11:07:44 +1200

    Add files via upload

---
+++ GCS_Github.py
@@ -0,0 +1,53 @@
+from pydub import AudioSegment
+import io
+import os
+from google.cloud import speech_v1p1beta1 as speech
+from google.cloud.speech_v1p1beta1 import enums
+from google.cloud.speech_v1p1beta1 import types
+import wave
+
+
+def mp3_to_wav(audio_file_name):
+    if audio_file_name.split('.')[1] == 'mp3':
+        sound = AudioSegment.from_mp3(audio_file_name)
+        audio_file_name = audio_file_name.split('.')[0] + '.wav'
+        sound.export(audio_file_name, format="wav")
+
+
+def frame_rate_channel(audio_file_name):
+    with wave.open(audio_file_name, "rb") as wave_file:
+        frame_rate = wave_file.getframerate()
+        channels = wave_file.getnchannels()
+        return frame_rate,channels
+
+def stereo_to_mono(audio_file_name):
+    sound = AudioSegment.from_wav(audio_file_name)
+    sound = sound.set_channels(1)
+    sound.export(audio_file_name, format="wav")
+
+filepath = "C:/Users/Nate.Liu/PycharmProjects/google-cloud-speech/"
+audio_file_name = "Nate.wav"
+file_name = filepath + audio_file_name
+
+mp3_to_wav(file_name)
+frame_rate, channels = frame_rate_channel(file_name)
+if channels > 1:
+    stereo_to_mono(file_name)
+
+client = speech.SpeechClient.from_service_account_json('My First Project-7ae222169690.json')
+
+# Loads the audio into memory
+with io.open('Nate.wav', 'rb') as audio_file:
+    content = audio_file.read()
+    audio = types.RecognitionAudio(content=content)
+
+config = types.RecognitionConfig(
+    encoding=enums.RecognitionConfig.AudioEncoding.LINEAR16,
+    sample_rate_hertz=frame_rate,
+    language_code='en-US')
+
+# Detects speech in the audio file
+response = client.recognize(config, audio)
+
+for result in response.results:
+    print('Transcript: {}'.format(result.alternatives[0].transcript))
